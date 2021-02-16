f6fabde652fe8250cb7ba670c369bf47e612694c
Quramy <yosuke.kurami@gmail.com>
2021-02-16 14:05:50 +0900

    feat: install styled component

--- src/app/components/AddTodo/index.tsx
+++ src/app/components/AddTodo/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { StyleSheet, Button, View, TextInput } from "react-native";
+import styled from "styled-components";
+import { Button, Text, View, TextInput, Dimensions } from "react-native";
 import { postTodo } from "../../apiClient";
 
 const AddTodo = () => {
@@ -9,25 +10,48 @@ const AddTodo = () => {
     setTodoTitle("");
   };
   return (
-    <View style={styles.container}>
-      <TextInput
-        autoFocus
-        value={todoTitle}
-        onChangeText={setTodoTitle}
-        placeholder="ここに何か書いてね"
-      />
-      <Button onPress={onPressSubmit} title="追加する" />
-    </View>
+    <Section>
+      <InlineInput>
+        <Label>やること</Label>
+        <TodoTitle
+          autoFocus
+          value={todoTitle}
+          onChangeText={setTodoTitle}
+          placeholder="ここに何か書いてね"
+        />
+      </InlineInput>
+      <ButtonArea>
+        <Button onPress={onPressSubmit} title="追加する" />
+      </ButtonArea>
+    </Section>
   );
 };
 
-export default AddTodo
+export default AddTodo;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const Section = styled(View)`
+  flex: 1;
+  align-items: center;
+`;
+
+const InlineInput = styled(View)`
+  width: ${Dimensions.get("window").width};
+  display: flex;
+  background: #fff;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 8;
+  padding-right: 8;
+`;
+
+const Label = styled(Text)`
+  width: 96;
+`;
+
+const TodoTitle = styled(TextInput)`
+  height: 48;
+`;
+
+const ButtonArea = styled(View)`
+  margin-top: 16;
+`;
